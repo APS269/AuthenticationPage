ea658e345f985fd8ad897c75ad7a4dd4e6e41de9
Abhay Pratap Singh <abhay26902@gmail.com>
2023-08-12 00:03:28 +0530

    level-4:Salting+hashing

--- app.js
+++ app.js
@@ -1,67 +1,71 @@
-//add .env as early as possible in the file
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const md5 = require("md5");
+const saltrounds = 10;
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-app.get("/",(req,res)=>
-    {
-    res.render("home");
-})
-mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
-
-//object from mongoose schema class
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
+const User = mongoose.model("User", userSchema);
 
-const User = new mongoose.model("User",userSchema);
-
-
+app.get("/", (req, res) => {
+    res.render("home");
+});
 
 app.get("/login", (req, res) => {
     res.render("login");
 });
+
 app.get("/register", (req, res) => {
     res.render("register");
 });
 
-app.post("/register", (req,res) =>
-{
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
-    newUser.save()
-        .then(() =>res.render("secrets"))
-        .catch((error) => console.log(error));
-})
+app.post("/register", (req, res) => {
+    bcrypt.hash(req.body.password, saltrounds, (err, hash) => {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save()
+            .then(() => res.render("secrets"))
+            .catch((error) => console.log(error));
+    });
+});
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
-    User.findOne({
-        email: username
-    })
-        .then((data) => { if (data.password == password) res.render("secrets"); else res.send("Wrong pass")})
+    User.findOne({ email: username })
+        .then((data) => {
+            if (data) {
+                bcrypt.compare(password, data.password, (err, result) => {
+                    if (result) {
+                        res.render("secrets");
+                    } else {
+                        res.send("Wrong password");
+                    }
+                });
+            } else {
+                res.send("User not found");
+            }
+        })
         .catch((error) => console.log(error));
-})
+});
 
-app.listen(3000, () => {console.log("Server started at 3000") });
-
-
+app.listen(3000, () => {
+    console.log("Server started at port 3000");
+});
